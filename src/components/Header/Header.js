72fd8b69de8c3836e69ab4019b6f170981ff4b83
Aminul Islam <aminulislamroqib@gmail.com>
2021-05-26 22:10:28 +0600

    upadte

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <Jumbotron>
+               <h1 className='tittle'>Know Your Team</h1>
+            </Jumbotron>
+        </div>
+    );
+};
+
+export default Header;
